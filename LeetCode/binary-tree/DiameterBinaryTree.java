5458b54256356cf62ede29203d2744a3f4df463b
hoangankin2211 <truonghuynhduchoang@gmail.com>
2024-03-06 14:26:05 +0700

    solved: diametr binary tre

---
+++ LeetCode/binary-tree/DiameterBinaryTree.java
@@ -0,0 +1,61 @@
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+
+ public class DiameterBinaryTree{
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        return Math.max(left + right,Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right)));
+    }
+
+    public int height(TreeNode node){
+        if (node == null) return 0;
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    
+    public static void main(String[] args) {
+               
+    }
+}
+
+
+//Solution For Zero Runtime
+class DiameterBinaryTreeV2{
+    int longestPath = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return longestPath;
+    }
+
+    public int height(TreeNode node){
+        if (node == null) return 0;
+        
+        int left = height(node.left)+1;
+        int right = height(node.right)+1;
+
+        longestPath = Math.max(longestPath,left + right);
+        
+        return Math.max(left, right) + 1;
+    }
+
+    
+    public static void main(String[] args) {
+               
+    }
+}
